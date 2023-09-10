c2064484af0c7ead072517a9e00d35f50ff313ce
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-10 20:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-// Your Script here.
-
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+	for(let i=0;i<decodedStr.length;i++){
+		const char = encodedStr[i];
+	}
+	if(/[A-Z]/.test(char)){
+		const charCode = char.charCodeAt(0);
+		if(charCode >= 65 && charCode <= 77) {
+			decodedArr.push(String.fromCharCode(charCode + 13));
+		}
+		else{
+			decodedArr.push(String.fromCharCode(charCode - 13));
+		}
+	}
+	else{
+		decodedArr.push(char);
+	}
+}
+return decodedArr.join(''); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
