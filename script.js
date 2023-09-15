6f08af003cd7f6a3da903506b601db60ea052c94
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-15 10:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	for(let i=0;i<decodedStr.length;i++){
-		const char = encodedStr[i];
-	}
-	if(/[A-Z]/.test(char)){
-		const charCode = char.charCodeAt(0);
-		if(charCode >= 65 && charCode <= 77) {
-			decodedArr.push(String.fromCharCode(charCode + 13));
-		}
-		else{
-			decodedArr.push(String.fromCharCode(charCode - 13));
-		}
-	}
-	else{
-		decodedArr.push(char);
-	}
+function rot13(encodedString) {
+  let decodedString = '';
+
+  for (let i = 0; i < encodedString.length; i++) {
+    const char = encodedString[i];
+
+    if (/[A-Z]/.test(char)) {
+      let charCode = encodedString.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) {
+        charCode = ((charCode - 65 - 13 + 26) % 26) + 65;
+      }
+
+      decodedString += String.fromCharCode(charCode);
+    } else {
+      decodedString += char;
+    }
+  }
+
+  return decodedString;
 }
-return decodedArr.join(''); //return decodedArr
-}
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+const encoded = "SERR PBQR PNZC";
+const decoded = rot13(encoded);
+console.log(decoded);
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
